96f1939fe6b087427a0f391b6cbd9ed43452f2ab
alvin zhao <yibozhao2018@gmail.com>
2020-09-21 11:52:37 -0700

    upcase downcase

--- ruby/sep21/advanced_methods_exercise/03_only_vowels.rb
+++ ruby/sep21/advanced_methods_exercise/03_only_vowels.rb
@@ -1,7 +1,12 @@
 # Write a method, `only_vowels?(str)`, that accepts a string as an arg.
 # The method should return true if the string contains only vowels.
 # The method should return false otherwise.
-
+def only_vowels?(str)
+    v = 'AEIOU'
+    arr = str.split('')
+    arr.all?{|c| v.include?(c.upcase)}
+    # upacase downcase include?
+end
 
 p only_vowels?("aaoeee")  # => true
 p only_vowels?("iou")     # => true
