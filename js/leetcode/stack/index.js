c6377dd5dfd41497a8696668bf186d07646e6cd8
alvin zhao <yibozhao2018@gmail.com>
2020-09-30 00:28:21 -0700

    leetcode

--- js/leetcode/stack/index.js
+++ js/leetcode/stack/index.js
@@ -4,3 +4,28 @@ stack.push(2);
 const item1 = stack.pop();
 const item2 = stack.pop();
 
+var isValid = function (s) {
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == "(" || s[i] == "{" || s[i] == "[") {
+      stack.push(s[i]);
+    } else {
+      if (
+        (s[i] == ")" && stack[stack.length - 1] == "(") ||
+        (s[i] == "]" && stack[stack.length - 1] == "[") ||
+        (s[i] == "}" && stack[stack.length - 1] == "{")
+      ) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
+
+// console.log(isValid('()'))
+// console.log(isValid("{[()]}"));
+// console.log(isValid("()[]"));
+// console.log(isValid("([)]"));
+
