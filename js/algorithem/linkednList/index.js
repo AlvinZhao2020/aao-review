67a0a26eea58357361f337f493e2ff0026733d34
alvin zhao <yibozhao2018@gmail.com>
2020-10-02 13:10:52 -0700

    lc 83

--- js/algorithem/linkednList/index.js
+++ js/algorithem/linkednList/index.js
@@ -20,3 +20,16 @@ var deleteNode = function (node) {
     node.val = node.next.val
     node.next = node.next.next
 }
+// leetcode 83 delete duplicated node in linkedn list
+var deleteDuplicates = function (head) {
+    let p = head;
+    while (p && p.next) {
+        if (p.next.val === p.val) {
+            p.next = p.next.next
+        } else {
+            p = p.next
+        }
+
+    }
+    return head
+};
