8ee68e2d1fc60f69142dacdd67eee528cf5e8338
alvin zhao <yibozhao2018@gmail.com>
2020-10-03 15:37:12 -0700

    myinstanceof

--- js/algorithem/linkednList/proto.js
+++ js/algorithem/linkednList/proto.js
@@ -1,3 +1,24 @@
 const obj = {};
 const func = () =>{};
 const arr = [];
+
+// instanceof  means can be find in the prototype linkedlist
+
+console.log(arr instanceof Array)
+console.log(arr instanceof Object)
+Object.prototype.x = 'x'
+console.log(arr.x)
+console.log(Array.x)
+
+const myInstanceOf = (obj,target) =>{
+    let p = obj
+    while(p){
+        if (p.__proto__ === target.prototype){
+            return true
+        }
+        p = p.__proto__
+    }
+    return false
+}
+
+console.log(myInstanceOf(arr,Object))
