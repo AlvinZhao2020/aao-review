baf3764ab9407bf387f66222419ad829cddfe864
alvin zhao <yibozhao2018@gmail.com>
2020-10-03 17:36:29 -0700

    lc206

--- js/algorithem/linkednList/index.js
+++ js/algorithem/linkednList/index.js
@@ -20,6 +20,19 @@ var deleteNode = function (node) {
     node.val = node.next.val
     node.next = node.next.next
 }
+// leetcode 206 reverse linked list
+var reverseList =(head)=>{
+    let p1 = head;
+    let p2 = null;
+    while(p1){
+        let temp = p1.next
+        p1.next = p2
+        p2 = p1
+        p1 = temp
+    }
+    return p2
+}
+
 // leetcode 83 delete duplicated node in linkedn list
 var deleteDuplicates = function (head) {
     let p = head;
